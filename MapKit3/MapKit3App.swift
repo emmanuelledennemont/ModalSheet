a6b7a8663a05eaa00c9c132e57150b09c2b5a077
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-30 13:40:30 +0200

    Initial Commit

---
+++ MapKit3/MapKit3App.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit3App.swift
+//  MapKit3
+//
+//  Created by Emmanuelle  Dennemont on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapKit3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
