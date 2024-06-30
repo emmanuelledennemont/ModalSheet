5e0781ab34e964280754c457a613e845973488ca
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-30 13:54:55 +0200

    first commit

--- MapKit3/ContentView.swift
+++ MapKit3/ContentView.swift
@@ -1,21 +1,41 @@
 //
 //  ContentView.swift
-//  MapKit3
+//  MapKit2
 //
-//  Created by Emmanuelle  Dennemont on 30/06/2024.
+//  Created by Emmanuelle  Dennemont on 18/06/2024.
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var showSheet: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack(alignment: .bottom) {
+            VStack {
+                Spacer()
+            }
         }
-        .padding()
+        .task {
+            showSheet = true 
+        }
+        .accentColor(.orange)
+        .sheet(isPresented: $showSheet) {
+            VStack(alignment: .leading, spacing: 10) {
+                Spacer().frame(height: 40)
+                
+                // Other content can be added here if needed
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .presentationDetents([.height(60), .medium, .large])
+            .presentationCornerRadius(20)
+            .presentationBackground(.regularMaterial)
+            .presentationBackgroundInteraction(.enabled(upThrough: .large))
+            .interactiveDismissDisabled()
+            .bottomMaskForSheet()
+        }
     }
 }
 
